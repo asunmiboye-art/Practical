23239ccf4c81343461d1a07fe6f6ba85044ae70e
Ayoola <asunmiboye@gmail.com>
2026-04-19 21:36:37 -0400

    staging

--- example.sh
+++ example.sh
@@ -2,9 +2,8 @@ Welcome to Landmark Technologies
 We are so exicted for this program.
 We are aming for great success!
 adding update for testing thanks
-<<<<<<< HEAD
+
 i will add this line
-=======
 i will add this line.
 adding update for testing thanks
->>>>>>> 47f6790 (rebase command)
+
