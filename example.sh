50a0e5c9ae3bef4ce9f74520ed4c95bc84428230
Ayoola <asunmiboye@gmail.com>
2026-04-19 21:39:38 -0400

    new update

--- example.sh
+++ example.sh
@@ -2,19 +2,10 @@ Welcome to Landmark Technologies
 We are so exicted for this program.
 We are aming for great success!
 adding update for testing thanks
-<<<<<<< HEAD
-i will add this line. 
-adding update for testing thanks
-
-new features add
-maven
-tomcayt
-sonacube
-
-=======
 
 i will add this line
 i will add this line.
 adding update for testing thanks
+sdECkajdcyogudycluycvLDCKUDC
+wdckutWKEVDJYvlweuycgLEYUCBWEC
 
->>>>>>> staging
