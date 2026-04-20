b9c17d92fcd226a6fd39ea2182a272052fffafe3
Ayoola <asunmiboye@gmail.com>
2026-04-19 21:29:29 -0400

    let learn something new

--- example.sh
+++ example.sh
@@ -2,5 +2,11 @@ Welcome to Landmark Technologies
 We are so exicted for this program.
 We are aming for great success!
 adding update for testing thanks
-i will add this line.
+i will add this line. 
 adding update for testing thanks
+
+new features add
+maven
+tomcayt
+sonacube
+
